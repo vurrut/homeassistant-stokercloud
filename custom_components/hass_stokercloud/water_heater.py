9cd784dfe513d7a751ee043f109ad624b62e94c8
vurrut <calvoredondo@gmail.com>
2025-01-05 13:12:38 +0100

    Update temp constant

--- custom_components/hass_stokercloud/water_heater.py
+++ custom_components/hass_stokercloud/water_heater.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from homeassistant.const import CONF_USERNAME, TEMP_CELSIUS
+from homeassistant.const import CONF_USERNAME
+from homeassistant.const import UnitOfTemperature
 import logging
 
 from homeassistant.components.water_heater import (
@@ -30,7 +31,7 @@ async def async_setup_entry(hass, config, async_add_entities):
 
 
 class StokerCloudWaterHeater(StokerCloudControllerMixin, WaterHeaterEntity):
-    _attr_temperature_unit = TEMP_CELSIUS
+    _attr_temperature_unit = UnitOfTemperature.CELSIUS
     _attr_supported_features = 0
 
     @property
